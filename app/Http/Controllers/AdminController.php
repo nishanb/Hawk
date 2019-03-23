22552906e47337adcac0cac8ff60ca3c261f37e1
Nishan <nishanb7@gmail.com>
2019-03-23 20:02:25 +0530

    Designed admin panel

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+  public function index(){
+      return view('admin.pages.index');
+  }
+
+  public function posts(){
+      return view('admin.pages.posts');
+  }
+
+  public function post($id){
+      return view('admin.pages.post');
+  }
+
+  public function users($id){
+      return view('admin.pages.users');
+  }
+
+  public function comments($id){
+      return view('admin.pages.comments');
+  }
+
+}
---
+++ resources/views/admin/inc/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="footer">
+  <div class="row align-items-center justify-content-xl-between">
+    <div class="col-xl-12">
+      <div class="copyright text-center text-xl-left text-muted">
+        &copy; 2019<a href="" class="font-weight-bold ml-1" target="_blank">Blog</a>
+      </div>
+    </div>
+
+  </div>
+</footer>
